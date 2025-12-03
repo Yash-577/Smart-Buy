c4233ca3e36dd9add769f943f9a2a248a74b5499
Yash Rastogi <yashrastogi090@gmail.com>
2025-12-03 22:57:19 +0530

    Import Home.css in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
+import "./Home.css";
+
 
 function Home() {
   const navigate = useNavigate();
