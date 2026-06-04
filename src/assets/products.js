cd8d012c62bb1326cf28ef50dc9d312e5f704ef9
Yash Rastogi <yashrastogi090@gmail.com>
2026-06-04 12:07:24 +0530

    Update product list with new items

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,6 +1,6 @@
 const Products =[
-  {id:1, name: "pc",price:15000, image: "https://via.placeholder.com/150" },
-  { id: 2, name: "Headphones", price: 2000, image: "https://via.placeholder.com/150" },
+  {id:1, name: "shirt1",price:15000, image: "	https://image.hm.com/assets/hm/83/d5/83d5612b7c0fd49f638a00ddd05bfb6672299ca6.jpg?imwidth=1536" },
+  { id: 2, name: "pant", price: 2000, image: "https://image.hm.com/assets/hm/6d/53/6d53c369f7b221e859f037612f66918fe6016763.jpg?imwidth=1536" },
   { id: 3, name: "Phone", price: 30000, image: "https://via.placeholder.com/150" },
   { id: 4, name: "Doormat", price: 1000, image: "https://via.placeholder.com/150" },
 ];
